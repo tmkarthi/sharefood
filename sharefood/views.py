5cb7c47e6bb82cb1ec7d4f2cd2e2800608c44400
Karthikeyan Marudhachalam <tmkarthi@gmail.com>
2013-12-21 14:25:44 +0530

    Endpoints for changing status of DonatedFood & FoodReservation are added. Still the transition validations need to be done.

--- sharefood/views.py
+++ sharefood/views.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import User
 from rest_framework import permissions
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from sharefood.models import DonatedFood, FoodReservation
 from sharefood.permissions import IsDonorOrReadOnly, IsDonorOrBeneficiaryOrReadOnly
@@ -25,10 +27,20 @@ Try it yourself by logging in as one of these four users: **amy**, **max**,
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,
                           IsDonorOrReadOnly,)
 
-    # @link(renderer_classes=(renderers.StaticHTMLRenderer,))
-    # def highlight(self, request, *args, **kwargs):
-    #     snippet = self.get_object()
-    #     return Response(snippet.highlighted)
+    def change_status(self, status):
+        donated_food = self.get_object()
+        donated_food.status = status
+        donated_food.save()
+        serializer = DonatedFoodSerializer(donated_food)
+        return Response(serializer.data)
+
+    @action()
+    def deliver(self, request, *args, **kwargs):
+        return self.change_status(DonatedFood.DELIVERED)
+
+    @action()
+    def cancel(self, request, *args, **kwargs):
+        return self.change_status(DonatedFood.CANCELLED)
 
     def pre_save(self, obj):
         obj.donor = self.request.user
@@ -65,6 +77,25 @@ Try it yourself by logging in as one of these four users: **amy**, **max**,
         food_donations_id = self.kwargs.get('donations_pk')
         obj.donation = DonatedFood.objects.get(pk=food_donations_id)
         obj.beneficiary = self.request.user
+
+    @action()
+    def accept(self, request, *args, **kwargs):
+        return self.change_status(FoodReservation.ACCEPTED)
+
+    @action()
+    def deliver(self, request, *args, **kwargs):
+        return self.change_status(FoodReservation.DELIVERED)
+
+    @action()
+    def cancel(self, request, *args, **kwargs):
+        return self.change_status(FoodReservation.CANCELLED)
+
+    def change_status(self, status):
+        food_reservation = self.get_object()
+        food_reservation.status = status
+        food_reservation.save()
+        serializer = FoodReservationSerializer(food_reservation)
+        return Response(serializer.data)
 
 
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
