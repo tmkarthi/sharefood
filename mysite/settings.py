515d8444b273097e4667d20518c9745e9c55ffa7
Marudhachalam, Karthikeyan <tmkarthi@gmail.com>
2014-12-21 17:08:16 +0530

    Request logging enabled

--- mysite/settings.py
+++ mysite/settings.py
@@ -87,3 +87,22 @@ STATIC_URL = '/static/'
 REST_FRAMEWORK = {
     'PAGINATE_BY': 10,
 }
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': './debug.log',
+        },
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
